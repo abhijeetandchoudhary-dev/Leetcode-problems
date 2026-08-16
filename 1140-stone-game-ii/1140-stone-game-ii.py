63ff5645d8947cdd01a5904b34af68231410f816
SudoCone-dev217 <1EW25IS001@ewit.edu.in>
2026-08-16 11:44:12 +0530

    Time: 85 ms (53.25%), Space: 23.1 MB (61.48%) - LeetHub

---
+++ 1140-stone-game-ii/1140-stone-game-ii.py
@@ -0,0 +1,35 @@
+class Solution:
+    def stoneGameII(self, piles):
+        n = len(piles)
+
+        suffix = [0] * (n + 1)
+        for i in range(n - 1, -1, -1):
+            suffix[i] = suffix[i + 1] + piles[i]
+
+        dp = {}
+
+        def dfs(i, m):
+            if i == n:
+                return 0
+
+            if i + 2 * m >= n:
+                return suffix[i]
+
+            if (i, m) in dp:
+                return dp[(i, m)]
+
+            best = 0
+
+            for x in range(1, 2 * m + 1):
+                if i + x > n:
+                    break
+
+                best = max(
+                    best,
+                    suffix[i] - dfs(i + x, max(m, x))
+                )
+
+            dp[(i, m)] = best
+            return best
+
+        return dfs(0, 1)
